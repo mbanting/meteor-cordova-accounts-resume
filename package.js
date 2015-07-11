1535ccb525f805adf17924500a15df9aa3093817
Marty Banting <mban@travelunderwriters.com>
2015-07-11 16:42:49 -0700

    Formatted and added comments to package.js

--- package.js
+++ package.js
@@ -1,56 +1,63 @@
+/**
+ * Basic package information
+ */
 Package.describe({
-  name: "mbanting:cordova-accounts-resume",
-  summary: "Ensures user sessions are resumed successfully on Meteor Cordova apps.",
-  version: "0.1.0",
-  git: "https://github.com/mbanting/meteor-cordova-accounts-resume"
+    name: "mbanting:cordova-accounts-resume",
+    summary: "Ensures user sessions are resumed successfully on Meteor Cordova apps.",
+    version: "0.1.0",
+    git: "https://github.com/mbanting/meteor-cordova-accounts-resume"
 });
 
-
+/**
+ * Cordova plugin dependencies
+ */
 Cordova.depends({
-  'org.apache.cordova.file': '1.3.3'
+    'org.apache.cordova.file': '1.3.3'
 });
 
+/**
+ * Configuration for usage
+ */
+Package.onUse(function (api) {
+    api.versionsFrom("1.0.1");
+    api.use(["meteor", "accounts-base"]);
+    api.use(["skinnygeek1010:cordova-status@0.2.0"], 'web.cordova');
 
-Package.onUse(function(api) {
-  api.versionsFrom("1.0.1");
-  api.use(["meteor", "accounts-base"]);
-  api.use(["skinnygeek1010:cordova-status@0.2.0"], 'web.cordova');
-
-  api.addFiles([
-      "lib/client/deviceStorageService.js",
-      "lib/client/index.js"
-      ], ["web.cordova"]
-  );
+    api.addFiles([
+            "lib/client/deviceStorageService.js",
+            "lib/client/index.js"
+        ], ["web.cordova"]
+    );
 
 });
 
-
+/**
+ * Configuration for testing
+ */
 Package.onTest(function (api) {
-  api.use(["tinytest", "accounts-password", "mbanting:cordova-accounts-resume"]);
+    api.use(["tinytest", "accounts-password", "mbanting:cordova-accounts-resume"]);
 
-  //override onUse to make the following available outside Cordova and in client environment for testing
-/*  api.use(["skinnygeek1010:cordova-status@0.2.0"], 'client');
-*/
+    // including "mbanting:cordova-accounts-resume" above should have worked
+    // but for some reason adding the files again is needed
+    api.addFiles([
+            "lib/client/deviceStorageService.js",
+            "lib/client/index.js"
+        ], ["web.cordova"]
+    );
 
-  // not sure why these need to be explicitly added for this to work
-  api.addFiles([
-        "lib/client/deviceStorageService.js",
-        "lib/client/index.js"
-      ], ["web.cordova"]
-  );
+    // export for access in tests
+    api.export('CordovaAccountsResume', 'web.cordova');
+    api.export('DeviceStorageService', 'web.cordova');
 
-  api.export('CordovaAccountsResume', 'web.cordova'); // export for testing purposes only
-  api.export('DeviceStorageService', 'web.cordova'); // export for testing purposes only
+    api.addFiles([
+            "tests/server/setup.js"
+        ], ["server"]
+    );
 
-  api.addFiles([
-        "tests/server/setup.js"
-      ], ["server"]
-  );
-
-  api.addFiles([
-    "tests/client/stubs.js",
-    "tests/client/cordovaAccountsResumeTests.js"
-    ], ["web.cordova"]
-  );
+    api.addFiles([
+            "tests/client/stubs.js",
+            "tests/client/cordovaAccountsResumeTests.js"
+        ], ["web.cordova"]
+    );
 
 });
